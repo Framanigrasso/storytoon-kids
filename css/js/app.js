90245491b6208591a663e6541197596c1d2b5844
Framanigrasso <64206480+Framanigrasso@users.noreply.github.com>
2025-06-20 11:35:03 +0200

    app.js

---
+++ css/js/app.js
@@ -0,0 +1,15 @@
+document.getElementById('recordButton').addEventListener('click', startRecording);
+document.getElementById('generateButton').addEventListener('click', generateAnimation);
+document.getElementById('viewButton').addEventListener('click', viewStory);
+
+function startRecording() {
+    alert('Inizio registrazione...');
+}
+
+function generateAnimation() {
+    alert('Generazione dell\'animazione in corso...');
+}
+
+function viewStory() {
+    alert('Visualizzazione della storia...');
+}
